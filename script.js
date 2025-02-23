33094b7d7d139055a3ff75c0268e811852b502cb
ulkustp <ulku.kstp@gmail.com>
2025-02-23 20:35:51 +0300

    Add files via upload
    Signed-off-by: ulkustp <ulku.kstp@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Web sitesi başarıyla yüklendi!");
+// Karanlık Mod Aç/Kapat
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+// Arama Fonksiyonu
+const searchBar = document.getElementById('search-bar');
+searchBar.addEventListener('keyup', function () {
+    let filter = searchBar.value.toLowerCase();
+    let items = document.querySelectorAll('.mimar, .yapi');
+
+    items.forEach(item => {
+        let text = item.innerText.toLowerCase();
+        if (text.includes(filter)) {
+            item.style.display = "";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
